330707ec5e7be2b0c87d13988d9cee90f8ee92fd
Nate Johnson <jax523@gmail.com>
2023-03-28 14:22:34 -0300

    final image update

--- R/testing.R
+++ R/testing.R
@@ -41,7 +41,7 @@ column.df$treatment <- factor(column.df$treatment, levels=c("Nitrate", "Drought"
 row.df <- read.delim("test/rows.txt")
 value.df <- read.delim("test/values.txt")
 
-# svglite::svglite("test/test.svg", 4.9, 5.4)
+svglite::svglite("test/test.svg", 7, 5.4)
 
 par(mar=c(5,7,5,10))
 nh = nheatmap(value.df, zero_centered_colors = T,
@@ -58,18 +58,18 @@ nh = nheatmap_group(nh, 3, 'treatment', labels=T, label_just = 'left',
 nh = nheatmap_annotate(nh, 3, 'tissue', label_just = 'left',
                        col=setNames('red', 'Root'))
 
-nh = nheatmap_group(nh, 4, 'PlantTFDB', labels=T, label_just = 'left', show_bounding_box = F)
-nh = nheatmap_group(nh, 4, 'Nit_GOs', labels=F, label_just = 'left', show_bounding_box = T)
+nh = nheatmap_group(nh, 4, 'PlantTFDB', labels=T, label_just = 'left')
+nh = nheatmap_group(nh, 4, 'Nit_GOs', labels=F, label_just = 'left')
 
 nh = nheatmap_names(nh, 2, cex=0.5)
 nh = nheatmap_names(nh, 2, 'symbol', cex=0.5)
 nh = nheatmap_dend(nh, 4, lwd=1.5, gap=0.4)
 
-# dev.off()
-# fix_svg('test/test.svg')
+dev.off()
+fix_svg('test/test.svg')
 
 # svglite::svglite("test/legend.svg", 4.9, 5.4)
-nheatmap_legend(nh)
+# nheatmap_legend(nh)
 # dev.off()
 # fix_svg('test/legend.svg')
 
