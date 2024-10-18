5facffa4381fc165d0477815675f86b3d2327ff6
Nate Johnson <jax523@gmail.com>
2024-10-18 11:18:25 -0300

    Including data for ND

---
+++ R/ND_data.R
@@ -0,0 +1,18 @@
+#' Test Nitrogen/Drought dataset
+#'
+#' Data from a meta-analsis of N and D projects in the SRA. L2FC data for a set of genes and libraries in a matrix. Includes values, and metadata for genes and libraries.
+#'
+#' @docType data
+#'
+#' @usage data(ND)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(ND)
+#'
+"ND"
